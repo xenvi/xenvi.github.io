4ab511c12a8a1978bdf3e422c0d1d5962a011db2
xenvi <iluvpups20@gmail.com>
2020-02-13 17:48:07 -0600

    jquery carousel added

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$(function() {
+$(document).ready(function() {
   // smooth scroll
   $("a").click(function(event) {
     if (this.hash !== "") {
@@ -34,13 +34,58 @@ $(function() {
   );
 
   $("#card1").click(function() {
-    $(".card1-open").fadeIn(500);
+    $(".card1-open")
+      .fadeIn(500)
+      .css("display", "flex");
   });
   $("#card2").click(function() {
-    $(".card2-open").fadeIn(500);
+    $(".card2-open")
+      .fadeIn(500)
+      .css("display", "flex");
   });
   $(".close").click(function() {
+    var currentImg = $(".current");
     $(".canvas").fadeOut(500);
+    currentImg.removeClass("current");
+    $(".slider-inner:first-child").addClass("current");
+  });
+
+  // carousel
+  var imageBox = $(".carousel ul"),
+    imageWidth = $(".carousel ul li")
+      .first()
+      .children("img")
+      .width(),
+    imageQuantity = $(".carousel ul").children("li").length,
+    currentImage = 1;
+  console.log(imageWidth);
+
+  imageBox.css("width", imageWidth * imageQuantity);
+
+  function transition(currentImageInput, imageWidthInput) {
+    var pxValue = -(currentImageInput - 1) * imageWidthInput;
+    imageBox.animate({
+      left: pxValue
+    });
+  }
+
+  $("#next").click(function() {
+    if (currentImage === imageQuantity) {
+      currentImage = 1;
+      transition(currentImage, imageWidth);
+    } else {
+      currentImage++;
+      transition(currentImage, imageWidth);
+    }
+  });
+  $("#prev").click(function() {
+    if (currentImage === 1) {
+      currentImage = imageQuantity;
+      transition(currentImage, imageWidth);
+    } else {
+      currentImage--;
+      transition(currentImage, imageWidth);
+    }
   });
 
   // send contact form
